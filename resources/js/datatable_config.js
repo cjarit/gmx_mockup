12c38ca5cd0441a4a871562303b6486fc7a31565
Cjarit <cjarit@gmail.com>
2018-07-26 12:03:29 +0700

    js config data-table

--- resources/js/datatable_config.js
+++ resources/js/datatable_config.js
@@ -1,3 +1,31 @@
-$(document).ready(function() {
-  $('.data-table').DataTable();
-} );
+$(document).ready(function () {
+  $('.gmx-data-table .data-table').DataTable({
+    "paging": true,
+    "scrollY": true,
+    "scrollX": true,
+    searching: false,
+    "dom": '<"table-header"lf>rt<"table-footer"pi><"clear-fix">',
+    fixedColumns: {
+      leftColumns: 1
+    },
+    "order": [],
+    "columnDefs": [{
+      "targets": 'no-sort',
+      "orderable": false,
+    }],
+    language: {
+      paginate: {
+        next: '<i class="fas fa-angle-right"></i>',
+        previous: '<i class="fas fa-angle-left"></i>',
+        first: '<i class="fas fa-angle-double-left"></i> First',
+        last: 'Last <i class="fas fa-angle-double-right"></i>'  
+      }
+    },
+    "pagingType": "full_numbers"
+  });
+  setTimeout(
+    function () {
+      $.fn.dataTable.tables( {visible: true, api: true} ).columns.adjust();
+    },
+    200);
+});
